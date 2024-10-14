55fe1c9b2a1ef924b1bdb9ff14bbc1eda1eeb739
SamanDev <saman.rojina@gmail.com>
2024-10-14 07:10:40 +0330

    ss

--- index.js
+++ index.js
@@ -69,28 +69,35 @@ const values = [
         value: 10,
     },
 ];
+const valuestest = [
+    {
+        card: "A",
+        value: [1, 11],
+        hasAce: true,
+    },
+    {
+        card: "2",
+        value: 2,
+    },
+    {
+        card: "3",
+        value: 3,
+    },
+];
 let deck = [];
 function getDeck() {
+    if(deck.length<20){
     // Loop through suite AND values
-    for (let s = 0; s < suit.length; s++) {
-        for (let v = 0; v < values.length; v++) {
-            let card = { suit: suit[s], value: values[v] };
-            deck.push(card);
-        }
-    }
-    for (let s = 0; s < suit.length; s++) {
-        for (let v = 0; v < values.length; v++) {
-            let card = { suit: suit[s], value: values[v] };
-            deck.push(card);
-        }
-    }
-    for (let s = 0; s < suit.length; s++) {
-        for (let v = 0; v < values.length; v++) {
-            let card = { suit: suit[s], value: values[v] };
-            deck.push(card);
+    for (let i = 0; i < 6; i++) {
+        for (let s = 0; s < suit.length; s++) {
+            for (let v = 0; v < values.length; v++) {
+                let card = { suit: suit[s], value: values[v] };
+                deck.push(card);
+            }
         }
     }
     shuffle(deck);
+}
 }
 const cacheDuration = 1000 * 60 * 60 * 24 * 365; // 1 year
 const guid = () => {
@@ -122,9 +129,6 @@ const games = [];
 const players = {};
 const spectators = {};
 
-let gameOn = null;
-let gameStart = null;
-
 const createSeats = (seats) => {
     var _seats = [];
     for (let i = 0; i < seats; i++) {
@@ -137,7 +141,7 @@ const createTable = (gameId, min, seats) => {
     games.push({
         id: gameId,
         players: createSeats(seats),
-
+        sideBets: [],
         dealer: {
             cards: [],
             hiddencards: [],
@@ -145,13 +149,14 @@ const createTable = (gameId, min, seats) => {
             hasAce: false,
             hasLeft: null,
         },
-        gameOn: gameOn,
-        gameStart: gameStart,
+        gameOn: false,
+        gameStart: false,
         currentPlayer: 10,
         min: min,
         seats: seats,
         startTimer: -1,
-        timer: null,
+        timer: -1,
+        idcode: guid(),
     });
 };
 
@@ -212,33 +217,141 @@ function sendToAll(payLoad) {
         }
     }
 }
+function getHigh(data) {
+    var hand = data;
+    try {
+        hand = data.toString();
+    } catch (error) {}
 
+    if (hand.indexOf("A") > -1 || hand == "14" || hand.indexOf("14") > -1) return 14;
+    if (hand.indexOf("K") > -1 || hand == "13" || hand.indexOf("13") > -1) return 13;
+    if (hand.indexOf("Q") > -1 || hand == "12" || hand.indexOf("12") > -1) return 12;
+    if (hand.indexOf("J") > -1 || hand == "11" || hand.indexOf("11") > -1) return 11;
+    if (hand.indexOf("T") > -1 || hand == "10" || hand.indexOf("10") > -1) return 10;
+    if (hand.indexOf("9") > -1) return 9;
+    if (hand.indexOf("8") > -1) return 8;
+    if (hand.indexOf("7") > -1) return 7;
+    if (hand.indexOf("6") > -1) return 6;
+    if (hand.indexOf("5") > -1) return 5;
+    if (hand.indexOf("4") > -1) return 4;
+    if (hand.indexOf("3") > -1) return 3;
+    if (hand.indexOf("2") > -1) return 2;
+    if (hand.indexOf("1") > -1) return 1;
+}
+function isStraight(ranks) {
+    ranks.sort();
+
+    return (ranks[0] + 1 == ranks[1] || (ranks[0] == 14 && ranks[1] == 2)) && ranks[1] + 1 == ranks[2];
+}
+
+function checkSideBets(gameId) {
+    const game = games.filter((game) => game.id === gameId)[0];
+    const players = game.players;
+    var dealerCard = game.dealer.cards[0];
+    players.forEach((c, i) => {
+        if (c?.bet > 0) {
+            var cards = c.cards;
+            c.sideppx = 0;
+
+            if (cards[0].value.card == cards[1].value.card) {
+                c.sideppx = 5;
+                if (cards[0].suit == cards[1].suit) {
+                    c.sideppx = 25;
+                } else if (cards[0].suit.replace("Club", "Spade").replace("Heart", "Diamond") == cards[1].suit.replace("Club", "Spade").replace("Heart", "Diamond")) {
+                    c.sideppx = 12;
+                }
+            }
+            c.side213x = 0;
+            var arrayStraight = [getHigh(cards[0].value.card), getHigh(cards[1].value.card), getHigh(dealerCard.value.card)];
+            if (cards[0].value.card == cards[1].value.card && cards[0].value.card == dealerCard.value.card) {
+                c.side213x = 30;
+                if (cards[0].suit == cards[1].suit && cards[0].suit == dealerCard.suit) {
+                    c.side213x = 100;
+                }
+            } else if (isStraight(arrayStraight)) {
+                c.side213x = 10;
+                if (cards[0].suit == cards[1].suit && cards[0].suit == dealerCard.suit) {
+                    c.side213x = 40;
+                }
+            } else if (cards[0].suit == cards[1].suit && cards[0].suit == dealerCard.suit) {
+                c.side213x = 5;
+            }
+        }
+    });
+
+    game.sideBets
+        .filter((sideBet) => sideBet?.mode == "PerfectPer")
+        .map(function (bet) {
+            var x = game.players[bet.seat]?.sideppx ? game.players[bet.seat].sideppx : 0;
+            //console.log(cards,bet)
+            if (x > 0) {
+                bet.x = "x" + x;
+                bet.win = bet.amount * x;
+
+                var data = { gameName: "BlackjackMulti", data: [{ username: bet.nickname, bet1: bet.win }], id: game.idcode + bet.seat + bet.mode + bet.nickname };
+                changeBalance("gamesEndGame", data);
+            }
+
+            //console.log(cards, bet);
+
+            //console.log()
+        });
+    game.sideBets
+        .filter((sideBet) => sideBet?.mode == "21+3")
+        .map(function (bet) {
+            var x = game.players[bet.seat]?.side213x ? game.players[bet.seat].side213x : 0;
+            //console.log(cards,bet)
+            if (x > 0) {
+                bet.x = "x" + x;
+                bet.win = bet.amount * x;
+
+                var data = { gameName: "BlackjackMulti", data: [{ username: bet.nickname, bet1: bet.win }], id: game.idcode + bet.seat + bet.mode + bet.nickname };
+                changeBalance("gamesEndGame", data);
+                //console.log(cards, bet);
+            }
+            //console.log()
+        });
+    startTableTimer(gameId, game.currentPlayer, true);
+}
 function startBetTimer(gameId, blnStart) {
     const game = games.filter((game) => game.id === gameId)[0];
 
     if (blnStart) {
-        game.startTimer = 9;
+        game.startTimer = 20;
+        const payLoad = {
+            method: "tables",
+            games: games,
+        };
+        sendToAll(payLoad);
     } else {
         game.startTimer = game.startTimer - 1;
+        const payLoad = {
+            method: "timer",
+            gameId: gameId,
+            sec: game.startTimer,
+        };
+        sendToAll(payLoad);
     }
-
-    const payLoad = {
-        method: "tables",
-        games: games,
-    };
-    sendToAll(payLoad);
 
     if (game.startTimer > -1) {
         setTimeout(() => startBetTimer(gameId, false), 1000);
     } else {
         startDeal(gameId);
+
         const players = game.players;
 
-        var data = { gameName: "BlackjackMulti", data: [], id: idcode };
-        players.forEach((c) => {
-            data.data.push({ username: c.nickname, bet1: c.bet });
+        players.forEach((c, i) => {
+            if (c?.bet > 0) {
+                var data = { gameName: "BlackjackMulti", data: [{ username: c.nickname, bet1: c.bet }], id: game.idcode + i };
+                changeBalance("gamesStartGame", data);
+            }
         });
-        const _dec = changeBalance("gamesStartGame", data);
+        const playersSide = game.sideBets;
+
+        playersSide.forEach((c) => {
+            var data = { gameName: "BlackjackMulti", data: [{ username: c.nickname, bet1: c.amount }], id: game.idcode + c.seat + c.mode + c.nickname };
+            changeBalance("gamesStartGame", data);
+        });
     }
 }
 function startTableTimer(gameId, seat, blnStart) {
@@ -249,16 +362,21 @@ function startTableTimer(gameId, seat, blnStart) {
         return false;
     }
     if (blnStart) {
-        game.timer = 19;
+        game.timer = 20;
+        const payLoad = {
+            method: "tables",
+            games: games,
+        };
+        sendToAll(payLoad);
     } else {
         game.timer = game.timer - 1;
+        const payLoad = {
+            method: "timer",
+            gameId: gameId,
+            sec: game.timer,
+        };
+        sendToAll(payLoad);
     }
-
-    const payLoad = {
-        method: "tables",
-        games: games,
-    };
-    sendToAll(payLoad);
 
     if (game.timer > -1) {
         if (game.currentPlayer != seat) {
@@ -299,14 +417,18 @@ function startDeal(gameId) {
     leftCard = leftCard + 1;
 
     addCardDealerfake(gameId, leftCard * 250);
+    
 }
 function addCard(gameId, playNum, timeout) {
     setTimeout(() => {
+        console.log(deck.length)
         const game = games.filter((game) => game.id === gameId)[0];
         game.players[playNum].cards.push(deck[0]);
         deck.shift();
         updateSumDeal(gameId);
-
+        if (game.gameOn == false) {
+            game.gameOn = true;
+        }
         if (game.players[playNum].sum == 21 && game.players[playNum].cards.length == 2) {
             game.players[playNum].blackjack = true;
             updateCurrentPlayer(gameId, playNum);
@@ -328,7 +450,7 @@ function addCardDealer(gameId, timeout, compare) {
         game.dealer.cards.push(deck[0]);
         deck.shift();
         updateSumDeal(gameId);
-       
+
         if (game.dealer.sum == 21 && game.dealer.cards.length == 2) {
             game.dealer.blackjack = true;
             winLoseComponents(gameId);
@@ -345,31 +467,27 @@ function addCardDealer(gameId, timeout, compare) {
             games: games,
         };
         sendToAll(payLoad);
-        
     }, timeout);
 }
 function addCardDealerfake(gameId, timeout, compare) {
     setTimeout(() => {
         const game = games.filter((game) => game.id === gameId)[0];
         game.dealer.hiddencards.push({
-            "suit": "Back",
-            "value": {
-              "card": "0",
-              "value": 0
-            }
-          });
-      
-        updateSumDeal(gameId);
-        
-            game.gameOn = true;
-        
+            suit: "Back",
+            value: {
+                card: "0",
+                value: 0,
+            },
+        });
 
+        updateSumDeal(gameId);
+       
         const payLoad = {
             method: "tables",
             games: games,
         };
         sendToAll(payLoad);
-        startTableTimer(gameId, game.currentPlayer, true);
+        checkSideBets(gameId);
     }, timeout);
 }
 function winLoseComponents(gameId) {
@@ -397,13 +515,19 @@ function winLoseComponents(gameId) {
             }
         }
     });
-    var data = { gameName: "BlackjackMulti", data: [], id: idcode };
-    game.players.forEach((c) => {
+    
+    game.players.forEach((c,i) => {
         if (c.win && c.win > 0) {
-            data.data.push({ username: c.nickname, bet1: c.win });
+            if (c.isDouble){
+                var data = { gameName: "BlackjackMulti", data: [{ username: c.nickname, bet1: c.win }], id: game.idcode + i+"2" };
+            }else{
+                var data = { gameName: "BlackjackMulti", data: [{ username: c.nickname, bet1: c.win }], id: game.idcode + i };
+            }
+          
+                changeBalance("gamesEndGame", data);
         }
     });
-    changeBalance("gamesEndGame", data);
+    
     setTimeout(() => {
         game.players.map(function (player, playNum) {
             if (game.players[playNum]?.bet) {
@@ -415,6 +539,8 @@ function winLoseComponents(gameId) {
 
                 game.players[playNum].sum = 0;
                 game.players[playNum].cards = [];
+                game.players[playNum].sideppx = 0;
+                game.players[playNum].side213x = 0;
             }
         });
         game.dealer = {
@@ -424,10 +550,13 @@ function winLoseComponents(gameId) {
             hasAce: false,
             hasLeft: null,
         };
+        game.sideBets = [];
         game.gameOn = false;
         game.gameStart = false;
         game.currentPlayer = 10;
-
+        game.idcode = guid();
+        game.timer = -1;
+        game.startTimer = 1;
         const payLoad = {
             method: "tables",
             games: games,
@@ -463,7 +592,7 @@ function updateSumDeal(gameId) {
     var haveAce = false;
     if (game.dealer?.cards) {
         game.dealer.cards.map(function (card) {
-            var _val = card.value.value;
+            var _val = card?.value?.value;
             if (typeof _val === "object" && _val !== null) {
                 haveAce = true;
                 _val = _val[0];
@@ -525,7 +654,7 @@ wss.on("connection", (ws) => {
                     theClient: theClient,
                 };
                 ws.send(JSON.stringify(userPayload));
-               
+
                 if (game.currentPlayer > seat) {
                     game.currentPlayer = seat;
                 }
@@ -541,6 +670,38 @@ wss.on("connection", (ws) => {
                 startBetTimer(gameId, true);
             }
         }
+        if (result.method === "sidebet") {
+            const gameId = result.gameId;
+            const seat = result.seat;
+            const amount = result.amount;
+            const theClient = result.theClient;
+            const mode = result.mode;
+            const game = games.filter((game) => game.id === gameId)[0];
+            var _sideBet = {
+                seat: seat,
+                amount: amount,
+                mode: mode,
+                nickname: theClient.nickname,
+                win: 0,
+                x: null,
+            };
+
+            game.players[seat].balance = game.players[seat].balance - amount;
+            theClient.balance = theClient.balance - amount;
+
+            const userPayload = {
+                method: "connect",
+                theClient: theClient,
+            };
+            ws.send(JSON.stringify(userPayload));
+            game.sideBets.push(_sideBet);
+
+            const payLoad = {
+                method: "tables",
+                games: games,
+            };
+            sendToAll(payLoad);
+        }
         if (result.method === "hit") {
             const gameId = result.gameId;
             const seat = result.seat;
@@ -554,9 +715,9 @@ wss.on("connection", (ws) => {
             const seat = result.seat;
 
             const game = games.filter((game) => game.id === gameId)[0];
-            var data = { gameName: "BlackjackMulti", data: [], id: idcode };
+            var data = { gameName: "BlackjackMulti", data: [{ username: game.players[seat].nickname, bet1: game.players[seat].bet }], id: game.idcode+seat+"2" };
 
-            data.data.push({ username: game.players[seat].nickname, bet1: game.players[seat].bet });
+           
 
             changeBalance("gamesStartGame", data);
             game.players[seat].bet = game.players[seat].bet * 2;
@@ -567,10 +728,9 @@ wss.on("connection", (ws) => {
             const gameId = result.gameId;
             const seat = result.seat;
             const game = games.filter((game) => game.id === gameId)[0];
-if(seat==game.currentPlayer){
-    updateCurrentPlayer(gameId, seat);
-}
-            
+            if (seat == game.currentPlayer) {
+                updateCurrentPlayer(gameId, seat);
+            }
         }
         if (result.method === "leave") {
             const gameId = result.gameId;
@@ -588,17 +748,16 @@ if(seat==game.currentPlayer){
         }
 
         if (result.method === "syncBalance") {
-            if(theClient?.nickname){
+            if (theClient?.nickname) {
                 theClient.balance = result.balance;
 
                 const payLoad = {
                     method: "connect",
-    
+
                     theClient: theClient,
                 };
                 ws.send(JSON.stringify(payLoad));
             }
-           
         }
     });
     // The ClientId
@@ -667,12 +826,9 @@ const getUser = async (token, clientId, theClient, ws) => {
     });
 };
 const changeBalance = async (com, data) => {
-    //console.log(com);
+   // console.log(com, data);
     const userdata = await getChipService(com, data);
 
-    if (com == "gamesEndGame") {
-        idcode = guid();
-    }
     //console.log(userdata);
 };
 
